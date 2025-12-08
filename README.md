14939bcd39b6122f503af4996b85abac3453b6b8
yujinimda <s0912135@gmail.com>
2025-12-08 23:37:58 +0900

    feat: 문제 폴더, 파일 생성

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # js-study
 자바스크립트 문제 풀기
+
+1. 현재 클로저, 스코프, this 바인딩 공부 후 문제 풀기 진행중
+2. 배열 고차함수(map/filter/reduce) 진행예정
+3. 비동기(Async/Await, Promise) 진행예정
+4. DOM & 이벤트 심화 진행예정
+5. 실전 미니 프로젝트 문제 (debounce, throttle, memoize)
---
+++ this-binding/01_박유진.js
@@ -0,0 +1,34 @@
+//아래 코드의 출력 결과를 순서대로 모두 맞추세요.
+//정답은 4줄로 적어주세요.
+
+var name = "Global";
+
+const user = {
+  name: "Yujin",
+  getName() {
+    return this.name;
+  },
+  getNameLater1() {
+    setTimeout(function () {
+      console.log("1:", this.name);
+    }, 0);
+  },
+  getNameLater2() {
+    setTimeout(() => {
+      console.log("2:", this.name);
+    }, 0);
+  },
+  getNameLater3() {
+    const fn = this.getName;
+    setTimeout(fn, 0);
+  },
+  getNameLater4() {
+    const fn = this.getName.bind(this);
+    setTimeout(fn, 0);
+  },
+};
+    
+user.getNameLater1();
+user.getNameLater2();
+user.getNameLater3();
+user.getNameLater4();
